84e345f60f816e2b3877abb2536169967f7137f5
Stradalec <pochtatipo6@gmail.com>
2025-04-21 08:23:16 +0700

    necessary changes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.sem2labandroid6"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.sem2labandroid6"
