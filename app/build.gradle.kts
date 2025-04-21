aa5a256bd684caf1d1f1c2f992e31c3be2560758
Stradalec <pochtatipo6@gmail.com>
2025-04-21 08:38:01 +0700

    another necessary changes in build.gradle.kts (this program can not do it itself just to make me suffer)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,8 +36,8 @@ android {
 }
 
 dependencies {
-
-    implementation(libs.androidx.core.ktx)
+    implementation("androidx.recyclerview:recyclerview:1.4.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
